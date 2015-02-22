29dbe39048cf78c79861f3cc2011f5712d79234c
Wayne Jaehwan Park <triones24@gmail.com>
2015-02-22 19:27:32 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -33,10 +33,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
+## Write a short comment describing this function
 
 cacheSolve <-function(x, ...){
-
+      ## Return a matrix that is the inverse of 'x'
+      
   inversproperty<-x$getinverse()
   
   if(!is.null(inversproperty)) {
